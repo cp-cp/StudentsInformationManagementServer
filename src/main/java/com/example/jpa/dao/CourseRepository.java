a3462cc0288e32a6680c0af3f7abe4ae06170740
cp-cp <wxymmyx@126.com>
2023-01-14 16:11:31 +0800

    coursedelete

--- src/main/java/com/example/jpa/dao/CourseRepository.java
+++ src/main/java/com/example/jpa/dao/CourseRepository.java
@@ -7,11 +7,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@Transactional
 public interface CourseRepository extends JpaRepository<Course,Integer> {
     Page<Course> findAll(Pageable pageable);
     @Query("select c from Course c where c.teacher.id = ?1")
